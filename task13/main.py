03d55a92f8ae6582cb0655e5c98fdd80a202f0a3
ВашÐAgafonov Maxim <Agafonov@example.com>
2024-11-18 22:17:48 +0300

    Первый коммит

---
+++ task13/main.py
@@ -0,0 +1,21 @@
+class CaesarsCipher:
+    def __init__(self):
+        self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?."
+        self.alphabet_len = len(self.alphabet)
+
+    def encrypt(self, text: str, key: int) -> str:
+        return ''.join(self.alphabet[(self.alphabet.index(char) + key) % self.alphabet_len]
+                       if char in self.alphabet else char for char in text)
+
+    def decrypt(self, text: str, key: int) -> str:
+        return ''.join(self.alphabet[(self.alphabet.index(char) - key) % self.alphabet_len]
+                       if char in self.alphabet else char for char in text)
+
+
+if __name__ == "__main__":
+    cipher = CaesarsCipher()
+    message = "o3zR v..D0?yRA0R8FR8v47w0ER4.R1WdC!sLF5D"
+
+    for key in range(len(cipher.alphabet)):
+        decrypted_message = cipher.decrypt(message, key)
+        print(f"{key}: {decrypted_message}")
